ad358b5c8018cea3d2686256df099e660acdef1e
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2023-12-12 12:41:25 +0100

    Adición de comentarios ejercicio 1

--- Ejercicio1/main.cpp
+++ Ejercicio1/main.cpp
@@ -4,12 +4,14 @@
 
 using namespace std;
 
+// Se crea una estructura para almacenar los datos de un estudiante
 struct Estudiante {
     char nombre[50];
     int edad;
     float promedio;
 };
 
+// Se crea una función para agregar un estudiante a la lista
 void agregarEstudiante(vector<Estudiante> &listaEstudiantes) {
     Estudiante nuevoEstudiante;
 
@@ -29,6 +31,7 @@ void agregarEstudiante(vector<Estudiante> &listaEstudiantes) {
     cout << "Estudiante agregado correctamente." << endl;
 }
 
+// Se crea una función para imprimir los datos de un estudiante
 void verEstudiantes(const vector<Estudiante> &listaEstudiantes) {
     cout << "Lista de estudiantes:" << endl;
     for (const Estudiante &estudiante : listaEstudiantes) {
@@ -36,6 +39,7 @@ void verEstudiantes(const vector<Estudiante> &listaEstudiantes) {
     }
 }
 
+// Se crea una función para eliminar un estudiante de la lista
 void eliminarEstudiante(vector<Estudiante> &listaEstudiantes, const char *nombre) {
     for (auto it = listaEstudiantes.begin(); it != listaEstudiantes.end(); ++it) {
         if (strcmp(it->nombre, nombre) == 0) {
@@ -49,6 +53,7 @@ void eliminarEstudiante(vector<Estudiante> &listaEstudiantes, const char *nombre
 }
 
 int main() {
+    // Se crea una lista de estudiantes, se agregan dos estudiantes y se imprimen
     vector<Estudiante> listaEstudiantes;
 
     agregarEstudiante(listaEstudiantes);
